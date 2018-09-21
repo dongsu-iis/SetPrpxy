209b7b81514e6023fe06a3defeb3e20a36334acf
dongsu-iis <dongsu.workout@outlook.jp>
2018-09-21 16:44:48 +0900

    ちょっと整形

--- FormSetProxy.cs
+++ FormSetProxy.cs
@@ -7,12 +7,14 @@ namespace SetPrpxy
 {
     public partial class FormSetProxy : Form
     {
-        string proxyUrl;
+
         string pacUrl = ConfigurationManager.AppSettings["pacUrl"];
         string server = ConfigurationManager.AppSettings["proxyServer"];
         string port = ConfigurationManager.AppSettings["proxyPort"];
         string user = ConfigurationManager.AppSettings["authUser"];
         string pass = ConfigurationManager.AppSettings["authPassword"];
+        string proxyUrl = "";
+
         RegistryKey registry = Registry.CurrentUser
                                .OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
 
@@ -20,14 +22,14 @@ namespace SetPrpxy
         public FormSetProxy()
         {
             InitializeComponent();
-            proxyUrl = "http://" + user + ":" + pass + "@" + server + ":" + port;
+            proxyUrl = $"http://{user}:{pass}@{server}:{port}";
         }
 
         private void BtnEnable_Click(object sender, EventArgs e)
         {
             try
             {
-                SetEnvironment(true);
+                SetEnvReg(true);
                 MessageBox.Show("プロキシが有効になりました", "Info");
             }
             catch (Exception ex)
@@ -41,7 +43,7 @@ namespace SetPrpxy
         {
             try
             {
-                SetEnvironment(false);
+                SetEnvReg(false);
                 MessageBox.Show("プロキシが無効になりました", "Info");
             }
             catch (Exception ex)
@@ -50,7 +52,11 @@ namespace SetPrpxy
             }
         }
 
-        private void SetEnvironment(bool isValid)
+        /// <summary>
+        /// 環境変数とレジストリを設定する
+        /// </summary>
+        /// <param name="isValid">プロキシOn/Off</param>
+        private void SetEnvReg(bool isValid)
         {
 
             if (isValid)
